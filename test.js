74c745fa1927699e94129ee0e3c561cd5fa32f2c
Alι Aryαɴ <85696485+AliAryanTech@users.noreply.github.com>
2023-04-02 10:05:49 +0500

    chore: running

--- test.js
+++ test.js
@@ -7,18 +7,22 @@ const getBuffer = async (url) =>
         await axios.get(url, {
             responseType: 'arraybuffer'
         })
-    ).data(async () => {
-        const pfp = await getBuffer('https://telegra.ph/file/59952c903fdfb10b752b3.jpg')
-        const image = await new Welcomer()
-            .setBackground('https://i.pinimg.com/originals/07/28/dc/0728dc400eca09632215055ff003d8bf.gif')
-            .setGIF(true)
-            .setAvatar(pfp)
-            .setName('AliAryanTech')
-            .setDiscriminator(1100)
-        const buffer = await image.generate()
+    ).data
 
-        writeFile('buffer.gif', buffer, (err) => {
-            if (err) throw err
-        })
-        console.log('successful')
-    })()
+const welcomer = async () => {
+  const avatar = await getBuffer('https://telegra.ph/file/59952c903fdfb10b752b3.jpg')
+  const image = await new Welcomer()
+    .setBackground('https://i.pinimg.com/originals/07/28/dc/0728dc400eca09632215055ff003d8bf.gif')
+    .setGIF(true)
+    .setAvatar(avatar)
+    .setName('AlιAryαɴTech')
+    .setDiscriminator(1100)
+    .setBlur(2)
+  const buffer = await image.generate()
+
+  writeFile('buffer.gif', buffer, (err) => {
+    if (err) throw err
+  })
+  console.log('successful')
+}
+welcomer()
